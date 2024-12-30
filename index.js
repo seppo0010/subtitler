3448e04b806c5a1e4d7b3a0234f61844d4d4b960
Sebastian Waisbrot <seppo0010@gmail.com>
2024-12-29 22:24:51 -0300

    fix

--- index.js
+++ index.js
@@ -15,12 +15,16 @@ const sourceDirectory = process.env.SUBTITLER_SRC_DIR.replace(/\/+$/, '') + '/'
 if (
   (!process.env.OSDB_QUERY && !process.env.OSDB_TMDB_ID) ||
     !process.env.OSDB_API_KEY ||
+    !process.env.OSDB_LANGUAGE ||
     !process.env.OSDB_USERNAME ||
     !process.env.OSDB_PASSWORD) {
-  process.stderr.write('missing OSDB_QUERY/OSDB_TMDB_ID, OSDB_API_KEY, OSDB_USERNAME or OSDB_PASSWORD\n')
+  process.stderr.write('missing OSDB_QUERY/OSDB_TMDB_ID, OSDB_API_KEY, OSDB_LANGUAGE, OSDB_USERNAME or OSDB_PASSWORD\n')
   process.exit(1)
 }
-const os = new OS({ apikey: process.env.OSDB_API_KEY })
+const os = new OS({
+  apikey: process.env.OSDB_API_KEY,
+  useragent: process.env.OSDB_USERAGENT,
+})
 const OpenSubtitles = new OS_API({
   useragent: process.env.OSDB_USERAGENT,
   username: process.env.OSDB_USERNAME,
@@ -34,17 +38,20 @@ const getSubtitleFromOpenSubtitle = async (path, episode) => {
   try {
     const searchTerms = {}
     if (process.env.OSDB_QUERY) searchTerms.query = process.env.OSDB_QUERY
-    if (process.env.OSDB_TMDB_ID) searchTerms.tmdb_id = process.env.OSDB_TMDB_ID
+    if (process.env.OSDB_TMDB_ID) searchTerms.tmdb_id = parseInt(process.env.OSDB_TMDB_ID, 10)
     const { data } = await os.subtitles({
       moviehash,
-      query: process.env.OSDB_QUERY,
       season_number: episode.season,
       episode_number: episode.episode,
       ...searchTerms
       // sending `languages` here should work but it throws an error
     })
     const sub = data.filter((row) => row.attributes.language === process.env.OSDB_LANGUAGE)[0]
-    if (!sub) return
+    if (!sub) {
+      process.stderr.write(`subtitle not found for ${path}\n`)
+      return
+    }
+
     const { link } = await os.download({
       file_id: sub.attributes.files[0].file_id
     })
@@ -52,7 +59,7 @@ const getSubtitleFromOpenSubtitle = async (path, episode) => {
     const text = await response.text()
     fs.writeFileSync(pathToSrt(path), text)
   } catch (e) {
-    process.stderr.write(`failed to fetch subtitle for ${path}: ${e.toString()}`)
+    process.stderr.write(`failed to fetch subtitle for ${path}: ${e.toString()}\n`)
   }
 }
 
@@ -84,7 +91,7 @@ const getSubtitleFromOpenSubtitle = async (path, episode) => {
             process.stderr.write(`skipping subtitle file ${fileStats.name}\n`)
             return
           }
-          if (fs.existsSync(pathToSrt(fileStats.name))) {
+          if (fs.existsSync(path.join(root, pathToSrt(fileStats.name)))) {
             process.stderr.write(`skipping video that has a subtitle ${fileStats.name}\n`)
             return
           }
